6ddd9d30ab51c4322bd73cb79625dff54d42eafa
A.J. Sethi <aditya.sethi@anu.edu.au>
2024-05-09 12:27:25 +1000

    update bam color code for performance and counter

--- color_bam/color_bam_from_list.py
+++ color_bam/color_bam_from_list.py
@@ -2,17 +2,15 @@ import argparse
 import pysam
 from pysam import AlignedSegment
 
-# validate the rgb color provided bt the user 
 def validate_color(color):
     parts = color.split(',')
     if len(parts) != 3:
-        raise ValueError("colour must be a comma-separated string of three integers between 0-255.")
+        raise ValueError("Colour must be a comma-separated string of three integers between 0-255.")
     for part in parts:
         if not (0 <= int(part) <= 255):
             raise ValueError("Each RGB value must be between 0 and 255.")
     return color
 
-# read list of read IDs 
 def read_read_ids(file_path):
     with open(file_path, 'r') as f:
         return {line.strip() for line in f if line.strip()}
@@ -25,18 +23,24 @@ def main():
     parser.add_argument("-color", required=True, help="RGB color to set for the specified read IDs")
 
     args = parser.parse_args()
-    
+
     read_ids = read_read_ids(args.rids)
     color = validate_color(args.color)
     
-    with pysam.AlignmentFile(args.ibam, 'rb') as infile, pysam.AlignmentFile(args.obam, 'wb', template=infile) as outfile:
+    modified_count = 0  # counter for modified reads
+    
+    with pysam.AlignmentFile(args.ibam, 'rb', threads=4) as infile, \
+         pysam.AlignmentFile(args.obam, 'wb', template=infile, threads=4) as outfile:
         for read in infile:
             if read.query_name in read_ids:
                 read.set_tag("YC", color)
-            outfile.write(read)
+                outfile.write(read)
+                modified_count += 1
 
-    pysam.sort("-o", f"{args.obam}.sorted.bam", args.obam)
-    pysam.index(f"{args.obam}.sorted.bam")
+    pysam.sort("-o", f"{args.obam}.sorted.bam", args.obam, threads=4)
+    pysam.index(f"{args.obam}.sorted.bam", threads=4)
+
+    print(f"Total reads modified: {modified_count}")
 
 if __name__ == "__main__":
     main()
