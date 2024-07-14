678bc971e17e8db797065fd0a698034bedc1ce8f
A.J. Sethi <aditya.sethi@anu.edu.au>
2024-07-14 10:38:24 +1000

    quantize polyA threshold in color bam from polyA; update BAM ph header

--- color_bam/color_bam_from_polyA.py
+++ color_bam/color_bam_from_polyA.py
@@ -1,7 +1,22 @@
+import os 
+import sys 
 import argparse
 import pysam
 
-def calculate_color(pt_value):
+# fetch ROGUE1 version for BAM header
+# TODO: move this to a python module 
+def get_version():
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    parent_dir = os.path.dirname(script_dir)
+    version_file = os.path.join(parent_dir, 'version.txt')
+    try:
+        with open(version_file, 'r') as f:
+            return f.read().strip()
+    except FileNotFoundError:
+        return "unknown"
+    
+# sliding scale for poly(A) color 
+def calculate_color_sliding(pt_value):
     
     low_threshold = 30
     high_threshold = 120
@@ -14,7 +29,6 @@ def calculate_color(pt_value):
     elif pt_value >= high_threshold:
         return "0,0,255"  # blue
     else:
-
         # scale pt_value to a 0-1 range
         scaled_value = (pt_value - low_threshold) / (high_threshold - low_threshold)
         
@@ -23,34 +37,87 @@ def calculate_color(pt_value):
         blue = int(255 * scaled_value)
         return f"{red},0,{blue}"
 
+# quantize poly(A) color for nascent and mature thresholds
+def calculate_color_quantized(pt_value, low_threshold, high_threshold):
+    
+    gray_color = "128,128,128"  # RGB for grey
+
+    if pt_value == -1:
+        return gray_color
+    if pt_value <= low_threshold:
+        return "255,0,0"  # red
+    elif pt_value >= high_threshold:    
+        return "0,0,255"  # blue
+    else:
+        return gray_color  # grey for values between low and high thresholds
+
 def main():
     parser = argparse.ArgumentParser(description="Color reads in a BAM file based on PT tag values.")
     parser.add_argument("-ibam", required=True, help="Input BAM file")
     parser.add_argument("-obam", required=True, help="Output BAM file")
+    parser.add_argument("-q", type=str, help="Comma-separated low and high thresholds for quantized poly(A) color")
 
     args = parser.parse_args()
-    
+
+    # check if quantized thresholds should be used 
+    use_quantized_polya_thresholds = False
+    if args.q:
+        try:
+            low_threshold, high_threshold = map(int, args.q.split(','))
+            use_quantized_polya_thresholds = True
+        except ValueError:
+            print("Error: -q argument should be two comma-separated integers")
+            return
+        
     modified_count = 0  # count of modified reads
     
-    with pysam.AlignmentFile(args.ibam, 'rb', threads=8) as infile, \
-         pysam.AlignmentFile(args.obam, 'wb', template=infile, threads=8) as outfile:
-        for read in infile:
-            try:
-                pt_value = read.get_tag("pt")
-            except KeyError:
-                pt_value = -1  # default PT value if not present
-            
-            color = calculate_color(pt_value)
-            read.set_tag("YC", color)
-            outfile.write(read)
-            modified_count += 1
+    # fetch ROGUE1 version 
+    version = get_version()
+    
+    # prepare bam header entry 
+    new_header_entry = {
+        'ID': 'ROGUE1',
+        'PN': 'ROGUE1',
+        'VN': version,
+        'CL': ' '.join(['python'] + sys.argv),
+        'DS': 'Colored reads based on PT tag values'
+    }
+    
+    with pysam.AlignmentFile(args.ibam, 'rb', threads=8) as infile:
+
+        # append to the PG tag in the header
+        header = infile.header.to_dict()
+        if 'PG' in header:
+            header['PG'].append(new_header_entry)
+        else:
+            header['PG'] = [new_header_entry]
+        
+
+        with pysam.AlignmentFile(args.obam, 'wb', header=header, threads=8) as outfile:
+            for read in infile:
+                try:
+                    pt_value = read.get_tag("pt")
+                except KeyError:
+                    pt_value = -1  # default PT value if not present
+                
+                if use_quantized_polya_thresholds:
+                    color = calculate_color_quantized(pt_value, low_threshold, high_threshold)
+                else:
+                    color = calculate_color_sliding(pt_value)
+                
+                read.set_tag("YC", color)
+                outfile.write(read)
+                modified_count += 1
 
     print(f"Sorting and indexing output BAM file")
-    
-    # index
     pysam.index(f"{args.obam}", threads=8)
 
     print(f"Total reads modified: {modified_count}")
+    if use_quantized_polya_thresholds:
+        print(f"Quantized polyA color: Red <= {low_threshold}, {low_threshold+1}-{high_threshold-1} Grey, Blue >= {high_threshold}")
+    else:
+        print("Scaled polyA color: Red <= 30, 31-119 intermediate, Blue >= 120")
+
 
 if __name__ == "__main__":
     main()
--- igv/create_bam_re_tag.py
+++ igv/create_bam_re_tag.py
@@ -1,7 +1,7 @@
 import argparse
 import pysam
 
-def calculate_last_mapped_position(read):
+def calculate_ends(read):
     """
     Calculate the last mapped position of a read.
     For reverse strand reads, this is the leftmost position (start).
@@ -11,38 +11,52 @@ def calculate_last_mapped_position(read):
     :return: The last mapped position as an integer
     """
     if read.is_reverse:
-        return read.reference_start + 1  
+        return -read.reference_end , -read.reference_start - 1 
     else:
-        return read.reference_end
+        return read.reference_start + 1, read.reference_end
+
+def calculate_aligned_bases(read):
+    """
+    Calculate the total number of aligned bases for a read.
+    
+    :param read: A pysam AlignedSegment object
+    :return: The total number of aligned bases as an integer
+    """
+    return sum([length for operation, length in read.cigartuples if operation in [0, 7, 8]])
 
 def main():
-
-    parser = argparse.ArgumentParser(description="Add RE tag to reads in a BAM file based on last mapped position.")
+    parser = argparse.ArgumentParser(description="Add RE and AL tags to reads in a BAM file.")
     parser.add_argument("-ibam", required=True, help="Input BAM file")
     parser.add_argument("-obam", required=True, help="Output BAM file")
 
     args = parser.parse_args()
     
     modified_count = 0  # count of modified reads
-    
 
     with pysam.AlignmentFile(args.ibam, 'rb', threads=48) as infile, \
          pysam.AlignmentFile(args.obam, 'wb', template=infile, threads=48) as outfile:
         for read in infile:
 
-            # last mapped position of the read
-            last_position = calculate_last_mapped_position(read)
+            first_position, last_position = calculate_ends(read)
             
-            # add  RE tag with the last mapped position
-            # RE tag allows sorting plus strand alignments by read end position in IGV, which is useful for visualizing pol II progression....
+            # add RE tag with the last mapped position
+            # RE tag allows sorting plus strand alignments by read end position in IGV, which is useful for visualizing pol II progression
             read.set_tag("RE", last_position, value_type='i')
+
+            # add RS tag with first mapped position
+            # RS tag allows sorting for minus strand alignments by pol II position 
+            read.set_tag("RS", first_position, value_type='i')
+
+            # Add AL tag with the total number of aligned bases     
+            aligned_bases = calculate_aligned_bases(read)
+            read.set_tag("AL", aligned_bases, value_type='i')
             
             outfile.write(read)
             modified_count += 1
 
     print(f"Indexing output BAM file")
     
-    # index bam 
+    # Index BAM file
     pysam.index(f"{args.obam}", threads=8)
 
     print(f"Total reads modified: {modified_count}")
