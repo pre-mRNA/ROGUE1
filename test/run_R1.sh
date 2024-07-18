6db9e072fae378c10b86b5cdf449168a6f0338a4
A.J. Sethi <aditya.sethi@anu.edu.au>
2024-07-18 16:18:20 +1000

    updated tests for R1

--- test/run_R1.sh
+++ test/run_R1.sh
@@ -14,6 +14,13 @@ export output="${out_dir}/output_table.tsv"
 time python3 ~/R1/R1.py -b ${bam} -g ${gtf} -o ${output} -p -j > ${out_dir}/ROGUE1-log.txt 2>&1
 
 
+# run R1 with index 
+time python3 ~/R1/R1.py -b ${bam} -g ${gtf} -o ${output} -p -j --index /g/data/lf10/as7425/2023_mrna-biogenesis-maps/analysis/204-06-17_ROGUE1-splicing-order/annotation_with_clusters > ${out_dir}/ROGUE1-log.txt 2>&1
+
+
+
+
+
 # run R1 while calculating modifications 
 time python3 ~/R1/R1.py -b ${bam} -g ${gtf} -o ${output} -m -j 
 wc -l ${output}
